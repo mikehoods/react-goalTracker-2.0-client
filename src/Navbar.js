67805431bdaeb3b9f02f77feaf68beb714895c0a
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-04-09 18:39:09 -0500

    Changed Navbar.js Links to NavLinks for active styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Goal Tracker</h1>
             <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/create">Add New Goal</Link>
+                <NavLink to="/" exact activeClassName="current">Home</NavLink>
+                <NavLink to="/create" exact activeClassName="current">Add New Goal</NavLink>
             </div>
 
         </nav>
