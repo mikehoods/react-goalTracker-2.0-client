7d600205182b9e9deb942c665592c6e2ba2ec93f
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-04-11 20:59:05 -0500

    Setup POST route for CreateGoal.js. Still working on Steps and Category logic/design

--- src/CreateGoal.js
+++ src/CreateGoal.js
@@ -1,34 +1,34 @@
 import { useState } from 'react';
-import { useForm, useFieldArray} from 'react-hook-form';
+// import { useForm, useFieldArray} from 'react-hook-form';
 
 const CreateGoal = () => {
     const [title, setTitle] = useState("")
-    const [difficulty, setDifficulty] = useState("")
-    const [priority, setPriority] = useState("")
-    // const [steps, setSteps] = useState(["", "", ""])
+    const [difficulty, setDifficulty] = useState("easy")
+    const [priority, setPriority] = useState("low")
+    const [step, setStep] = useState("")
 
-    const StepList = () => {
-        const { control, register } = useForm();
-        const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
-          control, // control props comes from useForm (optional: if you are using FormContext)
-          name: "steps", // unique name for your Field Array
-          // keyName: "id", default to "id", you can change the key name
-        });
+    // const StepList = () => {
+    //     const { control, register } = useForm();
+    //     const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
+    //       control, // control props comes from useForm (optional: if you are using FormContext)
+    //       name: "steps", // unique name for your Field Array
+    //       // keyName: "id", default to "id", you can change the key name
+    //     });
       
-        return (
-            <form>
-            <p>All steps</p>
-          {fields.map((field, index) => (
-            <input
-              key={field.id} // important to include key with field's id
-              {...register(`steps.${index}.value`)} 
-              defaultValue={field.value} // make sure to include defaultValue
-            />
-          ))}
+    //     return (
+    //         <form>
+    //         <p>All steps</p>
+    //       {fields.map((field, index) => (
+    //         <input
+    //           key={field.id} // important to include key with field's id
+    //           {...register(`steps.${index}.value`)} 
+    //           defaultValue={field.value} // make sure to include defaultValue
+    //         />
+    //       ))}
           
-          </form>
-        );
-      }
+    //       </form>
+    //     );
+    //   }
 
         // const stepList =
         // steps.map((step, index) => {
@@ -51,10 +51,26 @@ const CreateGoal = () => {
         //     )
         // })
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const steps = [{ "step": step, "complete": false }]
+        const achieved = false
+        const goal = { title, difficulty, priority, steps, achieved }
+        console.log(goal)
+
+        fetch('http://localhost:8000/goals', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(goal)
+        }).then(() => {
+            console.log('new goal added')
+        })
+    }
+
     return (
         <div className="create-goal">
             <h2>Create a New Goal</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-main">
                     <label>Goal name:</label>
                     <input
@@ -92,11 +108,12 @@ const CreateGoal = () => {
                     <input
                         type="text"
                         required
+                        onChange={(e) => setStep(e.target.value)}
                     />
                     <p className="add-step">+ Add Step</p>
                 </div>
                 {/* {stepList}             */}
-                <StepList />
+                {/* <StepList /> */}
                 <div className="create-goal-footer">
                     <button>Create Goal</button>
                 </div>
